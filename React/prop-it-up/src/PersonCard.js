a6af53cbf4b12ed83130a81e0974bbd5234c1914
Raghad Aburahma <raghad.sa201@gmail.com>
2024-07-29 14:26:11 +0300

    Putting it Together

--- React/prop-it-up/src/PersonCard.js
+++ React/prop-it-up/src/PersonCard.js
@@ -1,13 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Card from './Card'
-export default function PersonCard() {
+
+export default function PersonCard(props) {
+
+
+
   return (
     <div className='cards'>
+
+
         
         <Card name={'Raghad Abu rahma'} age={'18'} haircolor={'white'}/>
         <Card name={'Mohammad Tabakhna'} age={'48'} haircolor={'Black'}/>
-        <Card name={'Shatha Bast'} age={'18'} haircolor={'Gold'}/>
-        <Card name={'Muath Ademar'} age={'18'} haircolor={'Blue'}/>
+        <Card name={'Shatha Bast'} age={'20'} haircolor={'Gold'}/>
+        <Card name={'Muath Ademar'} age={'33'} haircolor={'Blue'}/>
+        
     
     </div>
   )
