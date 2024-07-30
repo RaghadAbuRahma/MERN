134abc26af384163404baee1ad49a7be11f42a8e
Raghad Aburahma <raghad.sa201@gmail.com>
2024-07-30 15:51:45 +0300

    moreForms

--- React/hook-form/src/components/UserForm.js
+++ React/hook-form/src/components/UserForm.js
@@ -1,17 +1,88 @@
 import React, { useState } from  'react';
     
 const UserForm = (props) => {
-    const [firstname, setFisrtname] = useState("");
+    const [firstname, setFirstname] = useState("");
     const [lastname, setLastname] = useState("");
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");  
     const [confirmPassword, setConfirmPassword] = useState(""); 
+    const [firstNameError, setFirstNameError] = useState("");
+    const [lastNameError, setLastNameError] = useState("");
+    const [emailError, setEmailError] = useState("");
+    const [passwordError, setPasswordError] = useState("");
+    const [confirmPasswordError, setConfirmPasswordError] = useState("");
+
+    const handleFirstName = (e) => {
+        setFirstname(e.target.value);
+        if(e.target.value.length === 0) {
+            setFirstNameError("First name is required!");
+        }
+        else if(e.target.value.length < 2) {
+            setFirstNameError("Field must be at least 2 characters!");
+        } else {
+            // an empty string is considered a "falsy" value
+            setFirstNameError("");
+        }
+    }
+    const handleLastName = (e) => {
+        setLastname(e.target.value);
+        if(e.target.value.length === 0) {
+            setLastNameError("last name is required!");
+        }
+        else if(e.target.value.length < 2) {
+            setLastNameError("Field must be at least 2 characters!");
+        } else {
+            // an empty string is considered a "falsy" value
+            setLastNameError("");
+        }
+    }
+
+    const handleEmail = (e) => {
+        setEmail(e.target.value);
+        if(e.target.value.length < 5) {
+            setEmailError("Field must be at least 5 characters!");
+        } else {
+            // an empty string is considered a "falsy" value
+            setEmailError("");
+        }
+    }
+
+    const handlePassword= (e) => {
+        setPassword(e.target.value);
+        if(e.target.value.length < 8){
+            setPasswordError("The password must be at least 8 characters.");
+        } else {
+            // an empty string is considered a "falsy" value
+            setPasswordError("");
+        }
+    }
+
+        const handleConfirmPassword = (e) => {
+            setConfirmPassword(e.target.value);
+            if(e.target.value  !== password){
+                setConfirmPasswordError("Passwords doesn't match");
+            } else {
+                // an empty string is considered a "falsy" value
+                setConfirmPasswordError("");
+            }
+
+
+
+    }
+
+
+
+
+
+
+
+
 
     
     const createUser = (e) => {
         // we must prevent the default refresh of the browser to keep our state from being reset
         e.preventDefault();
-        setFisrtname("");
+        setFirstname("");
         setLastname("");
         setEmail("");
         setPassword("");
@@ -34,25 +105,57 @@ const UserForm = (props) => {
             <h1>User Form</h1>
             <div class="form-group">
                 <label>First Name: </label> 
-                <input type="text" class="form-control" onChange={ (e) => setFisrtname(e.target.value) } value={ firstname }/>
+                <input type="text" class="form-control" onChange={ handleFirstName } value={firstname}/>
+                {
+                    firstNameError ?
+                    <p class="text-danger">{ firstNameError } </p> :
+                    ''
+                }
             </div>
+         
             <div class="form-group">
                 <label>Last Name: </label> 
-                <input type="text" class="form-control" onChange={ (e) => setLastname(e.target.value) } value={ lastname } />
+                <input type="text" class="form-control" onChange={ handleLastName } value={lastname}/>
+                {
+                    lastNameError ?
+                    <p class="text-danger">{ lastNameError }</p> :
+                    ''
+                }
             </div>
             <div class="form-group">
                 <label>Email Address: </label> 
-                <input type="text" class="form-control" onChange={ (e) => setEmail(e.target.value) } value={ email } />
+                <input type="text" class="form-control" onChange={ handleEmail } value={ email } />
+                {
+                    emailError ?
+                    <p class="text-danger">{ emailError }</p> :
+                    ''
+                }
             </div>
             <div class="form-group">
                 <label>Password: </label>
-                <input type="text" class="form-control" onChange={ (e) => setPassword(e.target.value) } value={ password }/>
+                <input type="text" class="form-control" onChange={ handlePassword } value={ password }/>
+                {
+                    passwordError ?
+                    <p class="text-danger">{ passwordError }</p> :
+                    ''
+                }
+
             </div>
             <div class="form-group">
                 <label>Confirm Password: </label>
-                <input type="text" class="form-control" onChange={ (e) => setConfirmPassword(e.target.value) } value={ confirmPassword }/>
+                <input type="text" class="form-control" onChange={ handleConfirmPassword } value={ confirmPassword }/>
+                {
+                    confirmPasswordError ?
+                    <p class="text-danger">{ confirmPasswordError }</p> :
+                    ''
+                }
             </div>
-            <input class="btn btn-info" type="submit" value="Create User" />
+            {
+                firstNameError || lastNameError || emailError || passwordError || confirmPasswordError ?
+                <input type="submit" value="Create user" disabled /> : 
+                <input type="submit" value="Create user" />
+            }
+       
         </form>
     );
 };
